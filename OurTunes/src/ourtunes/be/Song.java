04f198f8df8c0f4b37213feb8d81cc366dec2aaa
Truncatedshark <56824915+Truncatedshark@users.noreply.github.com>
2019-11-26 14:49:18 +0100

    Update Song.java

--- OurTunes/src/ourtunes/be/Song.java
+++ OurTunes/src/ourtunes/be/Song.java
@@ -16,6 +16,8 @@ public class Song {
     private String artist;
     
     private String genre;
+    
+    private String path;
 
     private int songID;
     
@@ -23,16 +25,54 @@ public class Song {
     private int time;
     
     
-    private Song(String name, String artist, String genre, int songID, int time){
+    private Song(String name, String artist, String genre, int songID, int time, String path){
         
-            
+        this.name = name;
         this.songID = songID;
         this.artist = artist;
         this.genre = genre;
         this.songID = songID;
         this.time = time;
+        this.path = path;
         
     
     
     }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getSongID() {
+        return songID;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
 }
